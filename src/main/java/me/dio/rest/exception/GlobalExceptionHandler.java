85bb3de789c5fd5efe92b79ec90481d3a6b60837
joao.ottoni <jvoo2016@gmail.com>
2025-02-23 12:44:31 -0300

    Tentativa de publicacao no railway

--- src/main/java/me/dio/rest/exception/GlobalExceptionHandler.java
+++ src/main/java/me/dio/rest/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package me.dio.rest.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +12,8 @@ import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<ApiErrorResponse> handleNoSuchElementException(NoSuchElementException ex, HttpServletRequest request) {
         return createResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request);
@@ -21,8 +25,11 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiErrorResponse> handleGenericException(IllegalArgumentException ex, HttpServletRequest request) {
-        return createResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Internal error.", request);
+    public ResponseEntity<ApiErrorResponse> handleGenericException(Exception ex, HttpServletRequest request) {
+        var message = "Unexpected error.";
+        logger.error(message, ex);
+
+        return createResponse(HttpStatus.INTERNAL_SERVER_ERROR, message, request);
     }
 
     private ResponseEntity<ApiErrorResponse> createResponse(HttpStatus responseStatus,
